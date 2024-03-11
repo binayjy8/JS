c8a9cf374bdbef98d18674a6be02e41e60066f96
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-03-11 22:30:12 +0530

    push code

--- practice.js
+++ practice.js
@@ -10,3 +10,11 @@ if (score >= 100) {
 } else {
     console.log("hard luck");
 }
+
+let str = "appendix";
+
+if((str[0] === 'a') && (str.length>3)) {
+    console.log("Good String");
+} else {
+    console.log("Bad String");
+}
