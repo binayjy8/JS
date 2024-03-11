8ddc8e063e2912a21888c00316c9d5676fef1133
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-03-11 16:20:35 +0530

    add code

--- practice.js
+++ practice.js
@@ -1,4 +1,10 @@
-let color = "red";
-if(color == "red") {
+let color = "purple";
+if(color === "red") {
     console.log("stop");
+} else if(color === "yellow") {
+    console.log("slow down");
+} else if(color === "green") {
+    console.log("go");
+}else{
+    console.log("You are Blind");
 }
