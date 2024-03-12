b96d63317752b3868ff4c93eb1cd4cee450866df
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-03-12 16:26:24 +0530

    code push

--- practice.js
+++ practice.js
@@ -37,3 +37,31 @@ switch(color){
         default:
             console.log("Light is broken");
 }
+
+let day  = 7;
+
+switch(day){
+    case 1:
+        console.log("sunday");
+        break;
+        case 2:
+        console.log("monday");
+        break;
+        case 3:
+        console.log("tuesday");
+        break;
+        case 4:
+        console.log("wednessday");
+        break;
+        case 5:
+        console.log("thusrday");
+        break;
+        case 6:
+        console.log("friday");
+        break;
+        case 7:
+        console.log("saturday");
+        break;
+        default:
+            console.log("wrong day") 
+}
